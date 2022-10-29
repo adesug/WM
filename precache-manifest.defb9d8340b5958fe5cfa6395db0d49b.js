e47a0314d5ab0d7cc0bc8ce1420a363c5ac0a868
adesug <adesugiantoro89@gmail.com>
2022-10-30 00:15:45 +0700

    Updates

---
+++ precache-manifest.defb9d8340b5958fe5cfa6395db0d49b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f721669c9430fafe5cee01becb94778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56959653838d7065afda",
+    "url": "/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "d6eddd4a0168a44587b1",
+    "url": "/static/css/main.f9390ddf.chunk.css"
+  },
+  {
+    "revision": "56959653838d7065afda",
+    "url": "/static/js/2.77f6c619.chunk.js"
+  },
+  {
+    "revision": "68f8cc17abdaef53ece568d77aa3fd76",
+    "url": "/static/js/2.77f6c619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eddd4a0168a44587b1",
+    "url": "/static/js/main.4b7ba31f.chunk.js"
+  },
+  {
+    "revision": "55d4eda960e8fdadb8bc",
+    "url": "/static/js/runtime-main.86a08199.js"
+  },
+  {
+    "revision": "e867a986f3bfa7f2bbca5121b9092a12",
+    "url": "/static/media/login.e867a986.png"
+  }
+]);
